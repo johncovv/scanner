cbcd0b3b5b10570acb68ca4025f160996404a688
John Covv <contato@johncovv.com>
2023-03-18 22:58:08 -0300

    Installing styled-components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-	return (
-		<Html lang="en">
-			<Head />
+export default class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
 
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
 }
-
