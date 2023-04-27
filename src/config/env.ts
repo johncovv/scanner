9628328e03496a22ca7f15881038fb2a42a036c5
John Covv <contato@johncovv.com>
2023-04-27 20:50:26 -0300

    Updating environment rule

--- src/config/env.ts
+++ src/config/env.ts
@@ -10,7 +10,7 @@ const environment = {
 const passportPassword = environment.passport.password;
 
 if (!passportPassword) throw new Error("PASSPORT_PASSWORD is not set on the environment");
-if (passportPassword.length < 64) throw new Error("PASSPORT_PASSWORD needs to be at least 64 characters long");
+if (passportPassword.length < 32) throw new Error("PASSPORT_PASSWORD needs to be at least 32 characters long");
 if (!environment.adminPassword) throw new Error("ADMIN_PASSWORD is not set on the environment");
 if (!environment.staticDir) throw new Error("STATIC_DIR is not set on the environment");
 
