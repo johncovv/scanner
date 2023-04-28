11b5c571db9f312bbd163cda9dfec27d2d71f4f7
John Covv <contato@johncovv.com>
2023-04-27 21:48:54 -0300

    Setting the default _document header and font family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -25,5 +25,26 @@ export default class MyDocument extends Document {
 		} finally {
 			sheet.seal();
 		}
+	}
+
+	render() {
+		return (
+			<Html lang="pt-br">
+				<Head>
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<meta charSet="utf-8" />
+					<link rel="icon" href="/favicon.ico" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" />
+					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+
+					<title>Risti Scanner</title>
+				</Head>
+
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
 	}
 }
